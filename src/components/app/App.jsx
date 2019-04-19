dd91efe991b7fd4effb39c735b9ac83c3f352bdb
voidiz <voidqt@hotmail.com>
2019-04-19 14:17:56 +0200

    fix redirect and white start page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Router, Switch, Redirect } from 'react-router-dom';
 import { Transition, config } from 'react-spring/renderprops';
+import { Spin } from 'antd';
 
 import * as account from '../../api/account/account';
 import * as utils from '../../api/utils/utils';
@@ -20,13 +21,16 @@ export default class App extends React.Component {
     this.state = {
       loggedIn: false,
       newUser: false,
+      done: false,
     }
   }
 
   async componentDidMount() {
     if (utils.getCookie('X-CSRF-Token')) {
       let check = await account.validateLoggedIn();
-      this.setState({ loggedIn: check });
+      this.setState({ loggedIn: check, done: true });
+    } else {
+      this.setState({ done: true });
     }
   }
 
@@ -57,41 +61,43 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <Router history={history}>
-        <Route render={({location}) => (
-            <Transition
-              native
-              config={config.slow}
-              keys={location.pathname}
-              items={location}
-              from={{ position: 'absolute', opacity: 0 }}
-              enter={{ position: 'absolute', opacity: 1 }}
-              leave={{ position: 'absolute', opacity: 0 }}
-            >
-              {(loc, state) => style => (
-                <Switch location={state === 'update' ? location : loc}>
-                  <Route exact path="/" render={() => 
-                    this.state.newUser ? <SetupPage style={style} onFinish={this.handleSetup} /> : 
-                    this.state.loggedIn ? <CalendarPage onLogout={this.handleLogout} style={style} /> : 
-                    <WelcomePage style={style} />} />
-                  <Route path="/register" render={() => <RegisterPage style={style} onLogin={this.handleLogin} />} />
-                  <Route path="/login" render={() => <LoginPage style={style} onLogin={this.handleLogin} />} />
+      !this.state.done ?
+        <Spin style={{ position: 'absolute', left: '50%', top: '50%'}} /> :
+        <Router history={history}>
+          <Route render={({location}) => (
+              <Transition
+                native
+                config={config.slow}
+                keys={location.pathname}
+                items={location}
+                from={{ position: 'absolute', opacity: 0 }}
+                enter={{ position: 'absolute', opacity: 1 }}
+                leave={{ position: 'absolute', opacity: 0 }}
+              >
+                {(loc, state) => style => (
+                  <Switch location={state === 'update' ? location : loc}>
+                    <Route exact path="/" render={() => 
+                      this.state.newUser ? <SetupPage style={style} onFinish={this.handleSetup} /> : 
+                      this.state.loggedIn ? <CalendarPage onLogout={this.handleLogout} style={style} /> : 
+                      <WelcomePage style={style} />} />
+                    <Route path="/register" render={() => <RegisterPage style={style} onLogin={this.handleLogin} />} />
+                    <Route path="/login" render={() => <LoginPage style={style} onLogin={this.handleLogin} />} />
 
-                  {/* TODO: Protected routes, currently dont work because the validate check doesnt finish in time */}
-                  <Route
-                    path="/tasks" 
-                    render={() => this.state.loggedIn ? <TaskPage style={style} /> :
-                      <Redirect to='/login' />} />
-                  <Route
-                    path="/settings"
-                    render={() => this.state.loggedIn ? <SettingsPage style={style} /> : 
-                      <Redirect to='/login' />} />
-                  {/* end protected routes */}
-                </Switch>
-              )}
-            </Transition>
-        )} />
-      </Router>
+                    {/* Protected routes */}
+                    <Route
+                      path="/tasks" 
+                      render={() => this.state.loggedIn ? <TaskPage style={style} /> :
+                        <Redirect to='/login' />} />
+                    <Route
+                      path="/settings"
+                      render={() => this.state.loggedIn ? <SettingsPage style={style} /> : 
+                        <Redirect to='/login' />} />
+                    {/* end protected routes */}
+                  </Switch>
+                )}
+              </Transition>
+          )} />
+        </Router>
     );
   }
 }
